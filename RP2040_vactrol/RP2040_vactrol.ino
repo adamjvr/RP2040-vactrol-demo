92919ed9e55dcd066bb4619a8e63388018be81ab
Adam Vadala-Roth <fenderstratman@gmail.com>
2024-05-20 02:04:31 -0400

    Simple Demo for Vactrols
    Simple test of vactrol being used as potentiometer via PWM signal. Two vactrols in series with two different PWM. One to set the bass resistance of voltage divider and the other for cycling resistance.

--- RP2040_vactrol/RP2040_vactrol.ino
+++ RP2040_vactrol/RP2040_vactrol.ino
@@ -1,15 +1,20 @@
 // This Arduino program generates a PWM signal on the Raspberry Pi Pico.
 // It varies the duty cycle of the PWM signal several times using a loop.
 
-// Define the PWM output pin
-const int pwmPin = 2; // Example pin, you can change it to any usable IO pin
+// Define the PWM output pins
+const int pwmPin = 16; // Example pin, you can change it to any usable IO pin
+const int pwmPin_base = 17;
 
 void setup() {
   // Set the pinMode of the PWM pin to OUTPUT
   pinMode(pwmPin, OUTPUT);
+  pinMode(pwmPin_base, OUTPUT);
 }
 
 void loop() {
+  // Set base resistance value
+  analogWrite(pwmPin_base, 127);
+  
   // Vary the duty cycle several times using a loop
   
   // Increase the duty cycle from 0 to 255 in steps of 20
